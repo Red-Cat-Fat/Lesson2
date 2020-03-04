dee2f74a9c32f548c4108cf647004e9de6667574
Red-Cat-Fat <boronnikov_a_i@mail.ru>
2020-03-04 17:31:18 +0300

    Сделал компонент для демонстрации обработки уничтожения объекта

---
+++ LessonUnityProject/Assets/Scripts/OnEnableOrDestroyObject/OnEnableAndDestroyMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class OnEnableAndDestroyMessage : MonoBehaviour
+{
+    [SerializeField]
+    private string _enableMessage;
+    [SerializeField]
+    private string _destroyMessage;
+
+    //этот метод вызовется когда объект станет активным в сцене
+    private void OnEnable()
+    {
+        Debug.Log(gameObject.name + " говорит при создании: \n" + _enableMessage);
+    }
+
+    //этот метод вызовется перед уничтожением объекта
+    private void OnDestroy()
+    {
+        Debug.Log(gameObject.name + " говорит перед смертью: \n" + _destroyMessage);
+    }
+}
