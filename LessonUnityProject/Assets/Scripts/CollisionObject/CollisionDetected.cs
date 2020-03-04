fc6a8d0bf15b2674e8e711d1b3e4765f1e62d19c
Red-Cat-Fat <boronnikov_a_i@mail.ru>
2020-03-04 16:29:35 +0300

    Сделал сцену для демонстрации колизии

---
+++ LessonUnityProject/Assets/Scripts/CollisionObject/CollisionDetected.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetected : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("С "+ gameObject.name + " столкнулся " + other.gameObject.name);
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        Debug.Log("С "+ gameObject.name + " соприкасается " + other.gameObject.name);
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        Debug.Log(gameObject.name + " и " + other.gameObject.name + " перестали соприкасаться");
+    }
+}
---
+++ LessonUnityProject/Assets/Scripts/CollisionObject/TriggerDetected.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDetected : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("В "+ gameObject.name + " вошёл " + other.gameObject.name);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Внутри "+ gameObject.name + " находится " + other.gameObject.name);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Из "+ gameObject.name + " вышел " + other.gameObject.name);
+    }
+}
