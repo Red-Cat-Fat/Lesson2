83d57f47f37318d7aeb5eb9de6d5f8887dde6d4a
Red-Cat-Fat <boronnikov_a_i@mail.ru>
2020-03-04 15:34:43 +0300

    Был создан проект и сцена с демонстрацией пользовательского управления

---
+++ LessonUnityProject/Assets/Scripts/UserInput/MovebleObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovebleObject : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Коеффицент для настройки чуствительности перемещения")]
+    private float _movebleCoef = 20f;
+    private bool _isMove;
+    private Vector3 _startObjectPosition;
+    private Vector3 _startCursorePosition;
+    
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //если в этом кадре ЛКМ была нажата
+        {
+            _isMove = true;
+            _startCursorePosition = Input.mousePosition;
+            _startObjectPosition = transform.position;
+        }
+
+        if (Input.GetMouseButtonUp(0)) //если в этом кадре ЛКМ была поднята
+        {
+            _isMove = false;
+        }
+
+        if (_isMove)
+        {
+            var mouseShift = Input.mousePosition - _startCursorePosition;
+            transform.position = _startObjectPosition + mouseShift/_movebleCoef;
+        }
+    }
+}
---
+++ LessonUnityProject/Assets/Scripts/UserInput/UserInputDemo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInputDemo : MonoBehaviour
+{
+    [Tooltip("Отследивание удержания кнопки")]
+    [SerializeField]
+    private KeyCode _checkKeyPress;
+    [Tooltip("Отследивание нажатия кнопки")]
+    [SerializeField]
+    private KeyCode _checkKeyDown;
+    [Tooltip("Отследивание отпускания кнопки")]
+    [SerializeField]
+    private KeyCode _checkKeyUp;
+    private void Update()
+    {
+        if (Input.GetKey(_checkKeyPress))
+        {
+            Debug.Log("В данный момент удерживается кнопка " + _checkKeyPress);
+        }
+
+        if (Input.GetKeyDown(_checkKeyDown))
+        {
+            Debug.Log("Была нажата кнопка " + _checkKeyDown);
+        }
+
+        if (Input.GetKeyUp(_checkKeyUp))
+        {
+            Debug.Log("Была отпущена кнопка " + _checkKeyUp);
+        }
+    }
+}
